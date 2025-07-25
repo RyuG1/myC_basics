7a2c88c57e03bf39ef5ebf2de33acf53ea55d205
name <E-mail>
2025-07-25 10:06:34 +0900

    day02 study
    pointer / struct / file io

--- code/12_struct_usage.c
+++ code/12_struct_usage.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
+struct Person {
+    char name[50];
+    int age;
+};
+
 int main() {
-
-
-
-
-
-
-
+    struct Person p1 = {"Alice", 25};
+    printf("name: %s, age: %d\n", p1.name, p1.age);
 
     
     return 0;
 }
+
+
+// struct 는 세미콜론 ; 붙여야함.
--- code/14_project_mini.c
+++ code/14_project_mini.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+// 성적 계산기
 int main() {
+    int score;
+    printf("wirte your score: ");
+    scanf("%d", &score);
 
-
-
-
-
-
-
-
-    
+    if(score >= 90){
+        printf("score is A\n");
+    } else if(score >=80){
+        printf("score is B\n");
+    } else if(score >=70){
+        printf("score is C\n");
+    } else if(score >=60){
+        printf("score is D\n");
+    } else {
+        printf("score is F\n");
+    }    
     return 0;
 }
